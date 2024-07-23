018fdbb0e8c0f274e9cb5537f33291bb683bee7c
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-07-22 23:33:15 -0300

    feat: Layout component

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,17 @@
+import { Footer, Navbar } from "../components";
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export { Layout };
