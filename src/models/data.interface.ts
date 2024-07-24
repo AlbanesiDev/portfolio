7203c765469e2a7f0389f5e9550c500ec7c95740
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-07-23 23:45:22 -0300

    update interface

--- src/models/data.interface.ts
+++ src/models/data.interface.ts
@@ -1,8 +1,19 @@
 export interface IData {
   about_me: IAboutMe;
   skills: ISkill[];
-  proyects: IProject[];
-  contact: any[];
+  projects: IProject[];
+  contact: IContact;
+}
+
+export interface IPersonalInfo {
+  label: string;
+  value: string;
+  sublabel?: string;
+}
+
+export interface IListSection {
+  title: string;
+  item: IPersonalInfo[];
 }
 
 export interface IAboutMe {
@@ -12,37 +23,61 @@ export interface IAboutMe {
   img: string;
   icon: string;
   pdf_url: string;
-  list: List[];
+  list: IListSection[];
 }
 
-export interface List {
-  title: string;
-  item: Item[];
-}
-
-export interface Item {
+export interface ISkill {
   label: string;
-  value: string;
-  sublabel?: string;
+  icon: string;
+}
+
+export interface IStack {
+  label: string;
+  icon: string;
+}
+
+export interface IImage {
+  src: string;
+  alt: string;
+}
+
+export interface IVideoPreview {
+  cover: string;
+  src: string;
+  alt: string;
 }
 
 export interface IProject {
   title: string;
   subtitle: string;
   description: string;
-  stack: string[];
-  img: Img[];
-  video_preview: string;
+  stack: IStack[];
+  img: IImage[];
+  video_preview: IVideoPreview;
   url_app: string;
   url_github: string;
 }
 
-export interface Img {
-  src: string;
-  alt: string;
+export interface IContactFormField {
+  name: string;
+  placeholder: string;
+  type: string;
 }
 
-export interface ISkill {
+export interface IContactForm {
+  name: IContactFormField;
+  email: IContactFormField;
+  message: IContactFormField;
+  btn: string;
+}
+
+export interface IContact {
+  social_btn: ISocialButton[];
+  email: string;
+  form: IContactForm;
+}
+export interface ISocialButton {
   label: string;
   icon: string;
+  url: string;
 }
