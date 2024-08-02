f3cf954d749b8bb9c595f4fe078689b87c96d696
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-08-01 23:43:36 -0300

    refactor: Custom scroll restoration

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,22 @@
-import { Outlet, ScrollRestoration } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Cursor, Footer, Navbar } from "../components";
-import { useIsMobile } from "../hooks";
+import { useIsMobile } from "../hooks/useIsMobile";
+import { useEffect } from "react";
 
 const Layout: React.FC = () => {
 	const isMobile = useIsMobile();
+	const location = useLocation();
+
+	useEffect(() => {
+		document.documentElement.scrollTo({
+			top: 0,
+			left: 0,
+			behavior: "instant",
+		});
+	}, [location.pathname]);
 
 	return (
 		<>
-			<ScrollRestoration />
 			{!isMobile && <Cursor />}
 			<Navbar />
 			<Outlet />
